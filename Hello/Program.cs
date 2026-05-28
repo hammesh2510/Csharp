46beb0111239cb99fc2b251690d2852de95602ef
hammesh2510 <hammesh2005@gmail.com>
2026-05-28 15:08:19 +0530

    Initial commit

---
+++ Hello/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = Console.ReadLine();
+            //Console.WriteLine(s);
+
+            //int a = 10;
+            //int b = 20;
+
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b); 
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+
+            //Console.WriteLine(a % b);
+
+            //string name = "hammesh";
+            //Console.WriteLine("hello " + name + " welcome to dot net.");
+
+            //long a = 100000000;
+            //Console.WriteLine(a);
+
+            //float f = 34.45F;
+            //double d = 35.35;
+
+            //Console.WriteLine(d);
+            //Console.WriteLine(f);
+
+            // type casting
+
+            // implicit type casting 
+
+            // char to int to long to float to double 
+
+            //int a = 5;
+            //double b = a; // here c# automatically convert the int to double for us 
+
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //char c = 'a';  
+            //long d = c;
+            //Console.WriteLine(d);
+
+
+
+            // explicit type casting
+
+
+
+
+            // BRO code -------------------------------------------------------------------------------------------------------------------------------------
+
+
+            //int x = 123;
+            //int y = 321;
+
+            //int z = x + y;
+
+            ////Console.WriteLine(z);
+
+            //int age = 20; // whole number
+            //double height = 300.5; // decimal number
+            //bool f = false;
+            //char ch = '@';
+            //string name = "hammesh";
+
+
+            ////Console.WriteLine("Hello "+ name);
+            ////Console.WriteLine("Your age is : " + age);
+            ////Console.WriteLine("Your height is : "+ height + "Cm");
+            ////Console.WriteLine("Your symbol is : " + ch);
+
+            //const double pi = 3.14159; // constant we can not change the value of pi 
+            ////Console.WriteLine(pi);
+
+
+            //// type casting
+
+            //double a = 3.14;
+            //int b = Convert.ToInt32(a);
+
+            //int c = 123;
+            //string d = Convert.ToString(c);
+
+            ////Console.WriteLine(b);
+            ////Console.WriteLine(a.GetType());
+            ////Console.WriteLine(b.GetType());
+            ////Console.WriteLine(d);
+            ////Console.WriteLine(d.GetType());
+
+
+
+            //string gg = "true";
+            //bool v = Convert.ToBoolean(gg);
+
+            ////Console.WriteLine(v);
+            ////Console.WriteLine(v.GetType());
+
+
+
+
+            //Console.WriteLine("what is your name ? ");
+            //string name=Console.ReadLine();
+
+
+
+            //Console.WriteLine("what is your age ?");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("Hello " + name);
+            //Console.WriteLine("your age is "+ age);
+
+
+            //Console.WriteLine("enter your age : ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("you can drive");
+            //}
+            //else if(age<0) Console.WriteLine("please enter a valid age");
+            //else
+            //{
+            //    Console.WriteLine("you can not drive");
+            //}
+
+
+            //Console.WriteLine("enter the day:");
+            //string day = Console.ReadLine();
+
+            //switch (day)
+            //{
+            //    case "monday":
+            //        Console.WriteLine("It's monday");
+            //        break;
+            //    case "tuesday":
+            //        Console.WriteLine("It's tuesday");
+            //        break;
+            //    case "wednesday":
+            //        Console.WriteLine("It's wednesday");
+            //        break;
+            //    case "thrusday":
+            //        Console.WriteLine("It's thrusday");
+            //        break;
+            //    case "friday":
+            //        Console.WriteLine("It's friday");
+            //        break;
+            //    case "saturday":
+            //        Console.WriteLine("It's saturday");
+            //        break;
+            //    case "sunday":
+            //        Console.WriteLine("It's sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine(day + " is not a day");
+            //        break;
+            //}
+
+
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //string[] cars = { "BMW", "redbull", "mercedes", "maclaren" };
+
+            //Console.WriteLine(cars[0]);
+            //Console.WriteLine(cars[1]);
+            //Console.WriteLine(cars[2]);
+            //Console.WriteLine(cars[3]);
+
+            //for(int i = 0; i < cars.Length; i++)
+            //{
+            //    Console.WriteLine(cars[i]);
+            //}
+
+            //foreach(string car in cars)
+            //{
+            //    Console.WriteLine(car);
+            //}
+
+
+            //int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //int n = nums.Length;
+
+            //for(int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine(nums[i]);
+            //}
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello from OOPS!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
